2b917e80cae02f48728e01152497621066ab3b50
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2020-01-19 14:04:58 -0700

    fingured out the IO issue on HackerRank

--- ruby/SockMerchant/program.rb
+++ ruby/SockMerchant/program.rb
@@ -1,5 +1,8 @@
 require 'pry'
 
 class Program
-  
+  # binding.pry
+  def sockMerchant(n, socks)
+    socks
+  end
 end
--- ruby/SockMerchant/program_test.rb
+++ ruby/SockMerchant/program_test.rb
@@ -1,10 +1,17 @@
 require "minitest/autorun"
-require "minitest/pride"
+require "minitest/emoji"
 require_relative "program"
 
 class ProgramTest < Minitest::Test
   def test_it_exists
     program = Program.new
     assert_instance_of Program, program
+  end
+
+  def test_program_input_format
+    program = Program.new
+    n = 3
+    ar = [10,20,20]
+    assert_equal [10,20,20], program.sockMerchant(n, ar)
   end
 end
