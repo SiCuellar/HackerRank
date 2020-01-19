936d8046562675e9efb86a90e3cbe04b9fcc1daa
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2020-01-19 14:30:39 -0700

    working pair macher

--- ruby/SockMerchant/program.rb
+++ ruby/SockMerchant/program.rb
@@ -3,6 +3,19 @@ require 'pry'
 class Program
   # binding.pry
   def sockMerchant(n, socks)
-    socks
+    sock_groups = Hash.new
+    socks.each do |s|
+      if !sock_groups.include?(s)
+        sock_groups[s] = 1
+      else
+        count = sock_groups[s]
+        sock_groups[s] = count + 1
+      end
+    end
+
+    pairs = 0
+    sock_groups.values.each { |v| pairs += v / 2}
+
+    pairs
   end
 end
--- ruby/SockMerchant/program_test.rb
+++ ruby/SockMerchant/program_test.rb
@@ -17,9 +17,25 @@ class ProgramTest < Minitest::Test
   end
 
   def test_program_can_return_all_socks
+    skip
     program = Program.new
     n = 3
     ar = [10,20,10]
     assert_equal [10,20,10], program.sockMerchant(n, ar)
+  end
+
+  def test_it_can_sort_sock_collection
+    skip
+    program = Program.new
+    n = 3
+    ar = [10,20,10]
+    assert_equal [10,10,20], program.sockMerchant(n, ar)
+  end
+
+  def test_it_can_return_total_number_of_pairs
+    program = Program.new
+    n = 3
+    ar = [10,20,10]
+    assert_equal 1, program.sockMerchant(n, ar)
   end
 end
