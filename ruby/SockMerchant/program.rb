ad74d78e72cd770a26e6257d9f398c2c04d1d355
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2020-01-19 14:40:52 -0700

    working pair counter using hash tables

--- ruby/SockMerchant/program.rb
+++ ruby/SockMerchant/program.rb
@@ -1,20 +1,23 @@
 require 'pry'
 
 class Program
-  # binding.pry
   def sockMerchant(n, socks)
-    sock_groups = Hash.new
+    sock_pairs = Hash.new
+
     socks.each do |s|
-      if !sock_groups.include?(s)
-        sock_groups[s] = 1
+      if !sock_pairs.include?(s)
+        sock_pairs[s] = 1
       else
-        count = sock_groups[s]
-        sock_groups[s] = count + 1
+        count = sock_pairs[s]
+        sock_pairs[s] = count + 1
       end
     end
 
     pairs = 0
-    sock_groups.values.each { |v| pairs += v / 2}
+
+    sock_pairs.values.each do |v|
+      pairs += v / 2
+    end
 
     pairs
   end
--- ruby/SockMerchant/program_test.rb
+++ ruby/SockMerchant/program_test.rb
@@ -32,10 +32,17 @@ class ProgramTest < Minitest::Test
     assert_equal [10,10,20], program.sockMerchant(n, ar)
   end
 
-  def test_it_can_return_total_number_of_pairs
+  def test_it_can_return_number_of_pairs_one
     program = Program.new
     n = 3
     ar = [10,20,10]
     assert_equal 1, program.sockMerchant(n, ar)
+  end
+
+  def test_it_can_return_pairs_multiple
+    program = Program.new
+    n = 9
+    ar = [10,20,20,10,10,30,50,10,20]
+    assert_equal 3, program.sockMerchant(n, ar)
   end
 end
