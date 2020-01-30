2b76b5ffc5e218bda2a988c7f897e2672e98cccd
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2020-01-30 11:48:59 -0700

    initial reapeated strings test

---
+++ ruby/repeated_strings/program_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative 'program'
+
+class ProgramTest < Minitest::Test
+  def test_it_exists
+    program = Program.new
+    assert_instance_of Program, program
+  end
+end
