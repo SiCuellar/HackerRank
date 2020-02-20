80d3daf73ef578ab5f98a084f542da1c1bcdb933
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2020-02-20 14:21:29 -0700

    working constraint checker

--- ruby/counting_valleys/program.rb
+++ ruby/counting_valleys/program.rb
@@ -1,5 +1,14 @@
 require 'pry'
 
 class Program
+  def step_converter(steps)
+    steps.chars
+  end
 
+  def constraint_checker(steps)
+    step_array = step_converter(steps)
+    if step_array.count > 2 && step_array.count < 1000000
+      true
+    end
+  end
 end
--- ruby/counting_valleys/program_test.rb
+++ ruby/counting_valleys/program_test.rb
@@ -6,4 +6,16 @@ class ProgramTest < Minitest::Test
     program = Program.new
     assert_instance_of Program, program
   end
+
+  def test_it_can_convert_steps_to_array
+    program = Program.new
+    steps = "UDU"
+    assert_equal ["U","D","U"], program.step_converter(steps)
+  end
+
+  def test_constraints_are_being_followed
+    program = Program.new
+    steps = "UDU"
+    assert_equal true, program.constraint_checker(steps)
+  end
 end
