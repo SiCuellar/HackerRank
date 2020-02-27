8a25ac65db52b8b1c395cb508b89432963900723
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2020-02-26 21:22:10 -0700

    tesing 2d string

--- ruby/2d_array/program_test.rb
+++ ruby/2d_array/program_test.rb
@@ -7,4 +7,23 @@ class ProgramTest < Minitest::Test
     program = Program.new
     assert_instance_of Program, program
   end
+
+  def test_it_can_convert_sample_input_to_arrays
+    program = Program.new
+    input = 1 1 1 0 0 0
+            0 1 0 0 0 0
+            1 1 1 0 0 0
+            0 0 2 4 4 0
+            0 0 0 2 0 0
+            0 0 1 2 4 0
+
+    expected =[[1,1,1,0,0,0],
+              [0,1,0,0,0,0],
+              [1,1,1,0,0,0],
+              [0,0,2,4,4,0],
+              [0,0,0,2,0,0],
+              [0,0,1,2,4,0]]
+
+    assert_equal expected, program.make_arrays
+  end
 end
